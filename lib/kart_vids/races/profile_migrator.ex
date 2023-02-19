b8c0df3024f1fc5f76372e3a5cd3775439ab6454
Matt Widmann <mgwidmann@gmail.com>
2023-02-19 01:09:10 -0500

    Limit migration to 10 at a time due to memory

--- lib/kart_vids/races/profile_migrator.ex
+++ lib/kart_vids/races/profile_migrator.ex
@@ -5,28 +5,35 @@ defmodule KartVids.Races.ProfileMigrator do
   alias KartVids.Races
   alias KartVids.Races.{Race, Racer}
 
+  @step 10
+
   def migrate(location_id) do
-    all_races =
-      from(r in Race,
-        join: z in Racer,
-        on: z.race_id == r.id,
-        where: r.location_id == ^location_id and is_nil(z.racer_profile_id),
-        order_by: {:desc, r.started_at}
-      )
-      |> Repo.all()
-      |> Repo.preload(:racers)
+    count = Repo.one(from r in Race, select: count(r.id))
 
-    for race <- all_races, racer <- race.racers do
-      {:ok, profile} =
-        Races.upsert_racer_profile(%{
-          nickname: racer.nickname,
-          photo: racer.photo,
-          fastest_lap_time: racer.fastest_lap,
-          fastest_lap_kart: racer.kart_num,
-          fastest_lap_race_id: race.id
-        })
+    for s <- 1..count//@step do
+      all_races =
+        from(r in Race,
+          join: z in Racer,
+          on: z.race_id == r.id,
+          where: r.location_id == ^location_id and is_nil(z.racer_profile_id),
+          limit: @step,
+          offset: ^s
+        )
+        |> Repo.all()
+        |> Repo.preload(:racers)
 
-      Races.update_racer(:system, racer, %{racer_profile_id: profile.id})
+      for race <- all_races, racer <- race.racers do
+        {:ok, profile} =
+          Races.upsert_racer_profile(%{
+            nickname: racer.nickname,
+            photo: racer.photo,
+            fastest_lap_time: racer.fastest_lap,
+            fastest_lap_kart: racer.kart_num,
+            fastest_lap_race_id: race.id
+          })
+
+        Races.update_racer(:system, racer, %{racer_profile_id: profile.id})
+      end
     end
   end
 end
