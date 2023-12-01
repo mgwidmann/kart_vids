ead02d2b4b15855825907e2a50cf3817dc93337c
Matt Widmann <mgwidmann@gmail.com>
2023-11-30 22:42:56 -0500

    Drop drop fastest lap time for kart if other karts have faster.

--- lib/kart_vids/karts.ex
+++ lib/kart_vids/karts.ex
@@ -4,7 +4,7 @@ defmodule KartVids.Karts do
   require Logger
   alias KartVids.Repo
   alias KartVids.Content.Location
-  alias KartVids.Races.{Racer, Race}
+  alias KartVids.Races.{Racer, Race, Kart}
 
   @large_std_dev 0.75
   @top_std_dev 25
@@ -31,7 +31,8 @@ defmodule KartVids.Karts do
         from(r in Racer, join: race in Race, on: r.race_id == race.id, where: r.location_id == ^location.id and r.kart_num == ^kart.kart_num and fragment("?::date >= ?", race.started_at, ^started_at), select: count(r.id))
         |> Repo.one()
 
-      {records, _dropped, mean, std_dev} = compute_stats(records, location)
+      fastest_time = get_fastest_kart_time_for_type(location, kart.kart_num, kart.type)
+      {records, _dropped, mean, std_dev} = compute_stats(records, fastest_time, location)
 
       %Racer{id: racer_id, fastest_lap: fastest_time} = records |> Enum.min_by(& &1.fastest_lap)
 
@@ -43,6 +44,16 @@ defmodule KartVids.Karts do
         fastest_racer_id: racer_id
       }
     end
+  end
+
+  def get_fastest_kart_time_for_type(%Location{adult_kart_min: adult_kart_min, adult_kart_max: adult_kart_max}, kart_num, :adult) do
+    from(k in Kart, join: r in Racer, on: r.id == k.fastest_racer_id, where: k.kart_num != ^kart_num and k.kart_num >= ^adult_kart_min and k.kart_num <= ^adult_kart_max, order_by: r.fastest_lap, limit: 1, select: r.fastest_lap)
+    |> Repo.one()
+  end
+
+  def get_fastest_kart_time_for_type(%Location{junior_kart_min: junior_kart_min, junior_kart_max: junior_kart_max}, kart_num, :junior) do
+    from(k in Kart, join: r in Racer, on: r.id == k.fastest_racer_id, where: k.kart_num != ^kart_num and k.kart_num >= ^junior_kart_min and k.kart_num <= ^junior_kart_max, order_by: r.fastest_lap, limit: 1, select: r.fastest_lap)
+    |> Repo.one()
   end
 
   defp started_at(%Location{adult_kart_reset_on: adult_kart_reset_on, junior_kart_reset_on: junior_kart_reset_on}, kart) do
@@ -65,7 +76,7 @@ defmodule KartVids.Karts do
 
   @max_limit 10
 
-  defp get_fastest_races(kart, location) do
+  def get_fastest_races(kart, location) do
     started_at = started_at(location, kart)
 
     from(r in Racer,
@@ -81,50 +92,54 @@ defmodule KartVids.Karts do
     |> Enum.take(@max_limit)
   end
 
-  defp compute_stats(records, location) do
+  def compute_stats(records, fastest_time, location) do
     mapper = & &1.fastest_lap
+
+    fastest_records_time = Enum.min_by(records, mapper).fastest_lap
 
     std_dev = compute_std_dev(records, location, nil, mapper)
 
-    new_records = records |> quality_filter(location, std_dev, mapper)
-    mean = compute_mean(new_records, location, std_dev, mapper)
-    # new_records = new_records |> quality_filter(location, std_dev, mapper)
+    std_dev_or_fastest_diff = Enum.max([std_dev, abs(fastest_time - fastest_records_time)])
+
+    new_records = records |> quality_filter(location, std_dev_or_fastest_diff, fastest_time, mapper)
+    mean = compute_mean(new_records, location, std_dev, fastest_time, mapper)
+    # new_records = new_records |> quality_filter(location, std_dev, fastest_time, mapper)
 
     std_dev = compute_std_dev(new_records, location, mean, mapper)
-    mean = compute_mean(new_records, location, std_dev, mapper)
+    mean = compute_mean(new_records, location, std_dev, fastest_time, mapper)
 
     dropped = records -- new_records
 
     {new_records, dropped, mean, std_dev}
   end
 
-  def quality_filter(records, location, std_dev, mapper \\ fn x -> x end)
+  def quality_filter(records, location, std_dev, fastest_kart_time, mapper \\ fn x -> x end)
 
-  def quality_filter(records, _location, nil, _mapper) do
+  def quality_filter(records, _location, nil, _fastest_kart_time, _mapper) do
     records
   end
 
-  def quality_filter(records, location, std_dev, mapper) do
+  def quality_filter(records, location, std_dev, fastest_kart_time, mapper) do
     records
     |> Stream.filter(&(mapper.(&1) > location.min_lap_time && mapper.(&1) < location.max_lap_time))
     |> Enum.sort_by(&mapper.(&1))
-    |> drop_larger_than_std_dev(std_dev, mapper)
+    |> drop_larger_than_std_dev(std_dev, fastest_kart_time, mapper)
   end
 
-  def drop_larger_than_std_dev(records, std_dev, mapper \\ fn x -> x end, acc_len \\ 0, acc \\ [])
+  def drop_larger_than_std_dev(records, std_dev, _fastest_kart_time, mapper \\ fn x -> x end, acc_len \\ 0, acc \\ [])
 
-  def drop_larger_than_std_dev([], _std_dev, _mapper, _acc_len, acc), do: Enum.reverse(acc)
-  def drop_larger_than_std_dev([item], _std_dev, _mapper, _acc_len, acc), do: Enum.reverse([item | acc])
+  def drop_larger_than_std_dev([], _std_dev, _fastest_kart_time, _mapper, _acc_len, acc), do: Enum.reverse(acc)
+  def drop_larger_than_std_dev([item], _std_dev, _fastest_kart_time, _mapper, _acc_len, acc), do: Enum.reverse([item | acc])
 
-  def drop_larger_than_std_dev([item, next | rest], std_dev, mapper, acc_len, acc) do
+  def drop_larger_than_std_dev([item, next | rest], std_dev, fastest_kart_time, mapper, acc_len, acc) do
     i = mapper.(item)
     n = mapper.(next)
 
     # If difference too large for lower times, don't add to acc. Don't drop higher end times
-    if acc_len < 15 && n - i > std_dev * (acc_len * 5 / 100 + 1) do
-      drop_larger_than_std_dev([next | rest], std_dev, mapper, 0, [])
+    if acc_len < 15 && n - i > std_dev * (acc_len * 5 / 100 + 1) && i < fastest_kart_time do
+      drop_larger_than_std_dev([next | rest], std_dev, fastest_kart_time, mapper, 0, [])
     else
-      drop_larger_than_std_dev([next | rest], std_dev, mapper, acc_len + 1, [item | acc])
+      drop_larger_than_std_dev([next | rest], std_dev, fastest_kart_time, mapper, acc_len + 1, [item | acc])
     end
   end
 
@@ -148,8 +163,8 @@ defmodule KartVids.Karts do
     end
   end
 
-  def compute_mean(times, location, std_dev, mapper \\ fn x -> x end) do
-    top = times |> Enum.sort_by(mapper) |> Enum.filter(&(mapper.(&1) > location.min_lap_time && mapper.(&1) < location.max_lap_time)) |> drop_larger_than_std_dev(std_dev, mapper) |> Enum.take(@top_std_dev)
+  def compute_mean(times, location, std_dev, fastest_kart_time, mapper \\ fn x -> x end) do
+    top = times |> Enum.sort_by(mapper) |> Enum.filter(&(mapper.(&1) > location.min_lap_time && mapper.(&1) < location.max_lap_time)) |> drop_larger_than_std_dev(std_dev, fastest_kart_time, mapper) |> Enum.take(@top_std_dev)
     top_count = length(top)
 
     top |> Stream.map(mapper) |> Enum.sum() |> Kernel./(top_count)
